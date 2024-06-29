18dedb1866e98b11e5a0fe0c3c8d060b49859a73
TLiLi HAMDI <tlilios@yahoo.com>
2024-06-29 19:13:04 +0200

    NavigationLink

--- MoonShot/MoonShot/MissionView.swift
+++ MoonShot/MoonShot/MissionView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
     let mission: Mission
+    let crew: [CrewMember]
     var body: some View {
         ScrollView {
             VStack {
@@ -25,18 +31,58 @@ struct MissionView: View {
                     Text(mission.description)
                 }
                 .padding(.horizontal)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(crew, id: \.role) { crewMember in
+                            NavigationLink {
+                                Text("Astronaut details")
+                            } label: {
+                                HStack {
+                                    Image(crewMember.astronaut.id)
+                                        .resizable()
+                                        .frame(width: 104, height: 72)
+                                        .clipShape(.capsule)
+                                        .overlay(
+                                            Capsule()
+                                                .strokeBorder(.white, lineWidth: 1)
+                                        )
+                                    VStack(alignment: .leading) {
+                                        Text(crewMember.astronaut.name)
+                                            .foregroundStyle(.white)
+                                            .font(.headline)
+                                        Text(crewMember.role)
+                                            .foregroundStyle(.secondary)
+                                    }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                    }
+                }
             }
             .padding(.bottom)
         }
         .navigationTitle(mission.displayName)
         .navigationBarTitleDisplayMode(.inline)
         .background(.darkBackGroundColor)
+        
+    }
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        self.crew = mission.crew.map{ member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name) ")
+            }
+        }
     }
 }
 
 #Preview {
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    return MissionView(mission: missions[0])
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    return MissionView(mission: missions[0], astronauts: astronauts)
         .preferredColorScheme(.dark)
     
 }
